2829d396f2aa732892c7e38fcba6147329358145
unknown <fncoder@gmail.com>
2018-04-08 16:14:31 +0200

    remove langs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,21 +3,11 @@ import PropTypes from 'prop-types';
 
 const propTypes = {
   handleSearch: PropTypes.func.isRequired,
-  toggleClass: PropTypes.func.isRequired,
-  statusLang: PropTypes.string.isRequired,
 };
 
 const Header = props => (
   <div className="header">
     <input onChange={props.handleSearch} className="input-search" type="text" placeholder="Search" />
-    <div className="languages">
-      <button onClick={props.toggleClass} type="button" className="btn-dropdown">English</button>
-      <ul className={`dropdown ${props.statusLang ? 'active' : 'unactive'}`}>
-        <li className="dropdown-item"><a className="dropdown-item__link" href="/">English</a></li>
-        <li className="dropdown-item"><a className="dropdown-item__link" href="/pl">Polski</a></li>
-        <li className="dropdown-item"><a className="dropdown-item__link" href="/de">Deutsch</a></li>
-      </ul>
-    </div>
   </div>
 );
 
