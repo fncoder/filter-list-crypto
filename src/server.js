972fe24d61f0cc0439b2f5d36e6e5ffd63a4578f
unknown <fncoder@gmail.com>
2018-04-08 17:03:00 +0200

    path to static files

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express();
 
-app.use(express.static(__dirname + '/static'));
+const staticFiles = __dirname + './../dist';
+
+app.use(express.static(staticFiles));
 
 const port = process.env.PORT || 3001
 
